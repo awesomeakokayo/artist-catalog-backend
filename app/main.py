29132eaf60336439645c058cfee8d6d4bd76300b
awesomeakokayo <awesomeakokayo@gmail.com>
2025-09-09 08:02:21 -0700

    modified:   app/main.py

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Artist Music Platform API")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*", "http://127.0.0.1:5500"],
+    allow_origins=["https://ambitionsmiler.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
